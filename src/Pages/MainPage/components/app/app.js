b793297bccabc6d7e155c38d0bfee55a96fb548a
muccurly <mirmiras04@mail.ru>
2020-11-09 13:21:01 +0600

    sss

--- src/Pages/MainPage/components/app/app.js
+++ src/Pages/MainPage/components/app/app.js
@@ -5,7 +5,7 @@ export default class App extends Component {
     render(){
       
     return (
-        <div className="main">
+        <div className="mainPage">
             
         </div>
     );
